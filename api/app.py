826eb4a5a12a9cce67a754025de764f2c8e6660d
19533-PisaniFrancesco <19533@studenti.marconiverona.edu.it>
2024-06-03 09:22:14 +0200

    finito

--- api/app.py
+++ api/app.py
@@ -1,23 +1,50 @@
 from flask import Flask, render_template, redirect, url_for, request
+import math
 
 app = Flask(__name__)
-strength = 1
-sex = 0
+
+timer = 0
+hue = 0
+
+
+
 @app.route("/")
 def home():
+    global timer
+    timer = 0
     return render_template("index.html")
 
-@app.route("/variable")
-def var():
-    user = "Geeksforgeeks"
-    return render_template("variable_example.html", name=user)
+@app.route("/mouse_entered", methods=["POST", "GET"])
+def moyse_entered():
+    return render_template("mouse_entered.html")
+
+@app.route("/buton", methods=["POST", "GET"])
+def buton():
+    return render_template("buton.html")
 
 @app.route("/clicked", methods=["POST", "GET"])
-def for_loop():
-    global sex
-    global strength
-    sex = sex + strength
-    return render_template("for_example.html", sex=sex)
+def clicked():
+    return render_template("clicked.html")
+
+@app.route("/timer", methods=["POST", "GET"])
+def time():
+    global timer
+    timer += 1
+    return render_template("timer.html", timer=timer)
+
+@app.route("/colors", methods=["POST", "GET"])
+def colors():
+    global hue
+    hue = (hue+60) %360
+    return render_template("colors.html", hue=hue)
+
+@app.route("/sigma", methods=["POST", "GET"])
+def sigma():
+    return render_template("sigma.html")
+
+@app.route("/agmis", methods=["POST", "GET"])
+def agmis():
+    return render_template("agmis.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000, host="127.0.0.1")
+    app.run(debug=True, port=5000, host="127.0.0.1")
